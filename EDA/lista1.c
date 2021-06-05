4853508e72e0176dc4806a294016e28a693a3057
antoniofavarinfreire <favarinfreire@hotmail.com>
2021-06-05 12:23:15 -0300

    Terminei uma parte, só falta o resto

--- EDA/lista1.c
+++ EDA/lista1.c
@@ -1,25 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void max_mim(int vet[], int tam, int *pMim, int *pMax);
-
 int main(int argc, char const *argv[]){
-    int tam;
-    printf("Digite o tamanho do vetor:");
-    scanf("%d", &tam);
-    int vet[tam];
-    int mim, max;
-    max_mim(vet[tam], tam, &mim, &max);
-    return 0;
-}
-
-void max_mim(int vet[], int tam, int *pMim, int *pMax){
-    int i;
-    for(i=0;i<tam;i++){
-        if(*pMax > vet[i]){
-            *pMax = vet[i];
-        }else if(*pMim < vet[tam]){
-            *pMim = vet[i];
-        }
+    int i, cont;
+    cont = 5;
+    float v;
+    for (i= 0 , i< cont; i++){
+        scanf("%d", &v[i]);
     }
+    return 0;
 }
