79df80fcfe5eed9af10bd9e70ba6e06c2f372be3
antoniofavarinfreire <favarinfreire@hotmail.com>
2021-07-05 22:33:43 -0300

    Limpando pastas e reestruturando o trabalho

--- EDA/Trabalho1/main.c
+++ EDA/Trabalho1/main.c
@@ -4,6 +4,19 @@
 #include "MatrizDinamica.h"
 
 int main(){
-    
+    char url[]="arquivo.txt",
+      ch;
+    int num=0;
+    FILE *arq;
+ 
+    arq = fopen(url, "r");
+    if(arq == NULL)
+        printf("Erro, nao foi possivel abrir o arquivo\n");
+    else
+    while( (ch=fgetc(arq))!= '\n' )
+        printf("%s ", &ch);
+        printf("\n");
+    fclose(arq);
+ 
     return 0;
 }
